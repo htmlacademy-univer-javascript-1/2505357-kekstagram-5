7daafe649d846c0bc7cdb21aae7b3c2cd33dee5c
Мария Чернова <chernova.marija.2005@gmail.com>
2024-09-27 18:25:12 +0500

    исправлены ошибки, возникшие при запуске проверок

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,25 @@
 function isLongerOrShoter(string, maxLength) {
   return string.length <= maxLength;
 }
+console.log(isLongerOrShoter('проверяемая строка', 20));
+console.log(isLongerOrShoter('проверяемая строка', 18));
+console.log(isLongerOrShoter('проверяемая строка', 10));
 
 function isPalindrom(string) {
-  let newString = string.replaceAll(' ', '').toUpperCase();
+  const newString = string.replaceAll(' ', '').toUpperCase();
   let invertedString = '';
   for (let i = newString.length - 1; i >= 0; i--) {
-    invertedString += newString[i]
+    invertedString += newString[i];
   }
   if (newString === invertedString) {
     return true;
   }
   return false;
 }
+console.log(isPalindrom('топот'));
+console.log(isPalindrom('ДовОд'));
+console.log(isPalindrom('Кекс'));
+console.log(isPalindrom('Лёша на полке клопа нашёл '));
 
 function getNumber(string) {
   string = string.toString();
@@ -22,8 +29,16 @@ function getNumber(string) {
       number += string[i];
     }
   }
-  if (parseInt(number)) {
-    return parseInt(number);
+  if (number.length > 0) {
+    return parseInt(number, 10);
   }
   return NaN;
 }
+console.log(getNumber('2023 год'));
+console.log(getNumber('ECMAScript 2022'));
+console.log(getNumber('1 кефир, 0.5 батона'));
+console.log(getNumber('агент 007'));
+console.log(getNumber('а я томат'));
+console.log(getNumber(2023));
+console.log(getNumber(-1));
+console.log(getNumber(1.5));
