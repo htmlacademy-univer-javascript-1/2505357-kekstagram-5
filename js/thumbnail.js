5bcc3abd2aa8be516eafab6ccdbbef001bc513a4
Мария Чернова <chernova.marija.2005@gmail.com>
2024-11-06 16:35:47 +0500

    добавлен пробел перед =

--- js/thumbnail.js
+++ js/thumbnail.js
@@ -6,7 +6,7 @@ const createThumbnail = ({url, description, comments, likes}) => {
   const thumbnail = template.cloneNode(true);
 
   thumbnail.querySelector('.picture__img').src = url;
-  thumbnail.querySelector('.picture__img').alt= description;
+  thumbnail.querySelector('.picture__img').alt = description;
   thumbnail.querySelector('.picture__comments').textContent = comments.length;
   thumbnail.querySelector('.picture__likes').textContent = likes;
 
