cf370386006a80259ab2f7db482df396c11594e7
Мария Чернова <chernova.marija.2005@gmail.com>
2024-11-28 23:37:18 +0500

    теперь кол-во комментариев обновляется

--- js/big-picture.js
+++ js/big-picture.js
@@ -7,7 +7,6 @@ const commentListElement = bigPictureElement.querySelector('.social__comments');
 const commentsLoader = document.querySelector('.comments-loader');
 const cancelButtonElement = bigPictureElement.querySelector('.big-picture__cancel');
 const liElement = commentListElement.querySelector('li');
-const commentCountAll = bigPictureElement.querySelector('.comments-count');
 
 let currentCount = 0;
 let comments = [];
@@ -39,7 +38,8 @@ const showComments = () => {
 
   commentListElement.innerHTML = '';
   commentListElement.append(fragment);
-  commentCountAll.textContent = comments.length;
+  bigPictureElement.querySelector('.social__comment-count').innerHTML =
+  `${currentCount} из <span class="comments-count">${comments.length}</span> комментариев`;
 
 };
 
