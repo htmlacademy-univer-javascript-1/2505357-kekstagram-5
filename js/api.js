6029d9fb5de612ebb319b93f44d5436bcf1afa71
Мария Чернова <chernova.marija.2005@gmail.com>
2024-12-21 14:43:28 +0400

    правки

--- js/api.js
+++ js/api.js
@@ -4,7 +4,7 @@ import { sortPhotos } from './filters-change.js';
 import { showAlert } from './util.js';
 
 const URL_DATA_GET = 'https://29.javascript.htmlacademy.pro/kekstagram/data';
-const URL_DATA_SEND = ' https://29.javascript.htmlacademy.pro/kekstagram/';
+const URL_DATA_SEND = 'https://29.javascript.htmlacademy.pro/kekstagram/';
 const SEND_METHOD = 'POST';
 
 const imagesFiltersElement = document.querySelector('.img-filters');
